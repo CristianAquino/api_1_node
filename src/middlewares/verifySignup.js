f682fd3c657e091aacac4489ed0d3b2953f74bea
cristianaquino <ycristian10@gmail.com>
2022-09-27 11:25:58 -0500

    verify

---
+++ src/middlewares/verifySignup.js
@@ -0,0 +1,29 @@
+const { roles: ROLES } = require("../models/Role");
+const User = require("../models/User");
+
+function checkRolesExisted(req, res, next) {
+  const { roles } = req.body;
+  if (roles) {
+    for (x of roles) {
+      if (!ROLES.includes(x.name)) {
+        return res.json({ message: `se ingresaron roles que no existen` });
+      }
+    }
+  }
+  next();
+}
+
+async function checkDuplicateUsernameOrEmail(req, res, next) {
+  const { username, email } = req.body;
+  const user = await User.findOne({ username });
+  const userEmail = await User.findOne({ email });
+  if (user)
+    return res
+      .status(400)
+      .json({ message: `el username: ${username} ya existe` });
+  if (userEmail)
+    return res.status(400).json({ message: `el email: ${email} ya existe` });
+
+  next();
+}
+module.exports = { checkRolesExisted, checkDuplicateUsernameOrEmail };
