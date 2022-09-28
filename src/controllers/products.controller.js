8e7e5c72778e5e6c30b6b1f8f3392e59ddc8213b
cristianaquino <ycristian10@gmail.com>
2022-09-28 17:15:27 -0500

    products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,31 +1,57 @@
 const Product = require("../models/Product");
 const { uploadImage, deleteImage } = require("../libs/cloudinary");
 
-function createProducts(req, res) {
+function createProducts(req, res, next) {
   const { name, category, price, imgUrl } = req.body;
   const newProduct = new Product({ name, category, price, imgUrl });
-  newProduct.save().then((productSave) => res.status(201).json(productSave));
+  newProduct
+    .save()
+    .then((productSave) => res.status(201).json(productSave))
+    .catch((error) => next(error));
 }
-function getProduct(req, res) {
-  Product.find({}).then((products) => res.status(200).json(products));
+function getProduct(req, res, next) {
+  Product.find({})
+    .then((products) => res.status(200).json(products))
+    .catch((error) => next(error));
 }
-function getProductById(req, res) {
+function getProductById(req, res, next) {
   const { id } = req.params;
-  Product.findById(id).then((product) => res.status(200).json(product));
+  Product.findById(id)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({ message: "no se encontro producto" });
+      }
+      return res.status(200).json(product);
+    })
+    .catch((error) => next(error));
 }
 
-function updateProductById(req, res) {
+function updateProductById(req, res, next) {
   const { id } = req.params;
   const { body } = req;
-  Product.findByIdAndUpdate(id, body, { new: true }).then((update) =>
-    res.status(202).json(update)
-  );
+  Product.findByIdAndUpdate(id, body, { new: true })
+    .then((update) => {
+      if (!update) {
+        return res
+          .status(404)
+          .json({ message: "no se encontro el producto a actualizar" });
+      }
+      return res.status(202).json(update);
+    })
+    .catch((error) => next(error));
 }
-function deleteProductById(req, res) {
+function deleteProductById(req, res, next) {
   const { id } = req.params;
-  Product.findByIdAndDelete(id).then(() =>
-    res.status(200).json({ message: `product with id: ${id} deleted` })
-  );
+  Product.findByIdAndDelete(id)
+    .then((del) => {
+      if (!del) {
+        return res.status(404).json({ message: "no existe el producto" });
+      }
+      return res
+        .status(200)
+        .json({ message: `product with id: ${id} deleted` });
+    })
+    .catch((error) => next(error));
 }
 
 async function sendImage(req, res) {
--- src/models/Product.js
+++ src/models/Product.js
@@ -2,10 +2,10 @@ const { Schema, model } = require("mongoose");
 
 const productSchema = new Schema(
   {
-    name: String,
-    category: String,
-    price: Number,
-    imgUrl: String,
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+    imgUrl: { type: String, required: true },
   },
   {
     timestamps: true,
