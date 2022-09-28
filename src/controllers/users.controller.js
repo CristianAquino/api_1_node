ea4034348e206f64cb7b94385d45b3bdab6f635f
cristianaquino <ycristian10@gmail.com>
2022-09-28 17:15:58 -0500

    users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,25 @@
 const User = require("../models/User");
+const { rol: Role } = require("../models/Role");
 
-function createUser(req, res) {
-  res.json({ message: "creado" });
+async function createUser(req, res) {
+  const { username, email, password, roles } = req.body;
+
+  if (!roles)
+    return res.status(404).json({ message: "es necesario ingresar un rol" });
+
+  const newUser = new User({
+    username,
+    email,
+    password: await User.encryptPassword(password),
+  });
+
+  const foundRoles = await Role.find({ name: { $in: roles } });
+  newUser.roles = foundRoles.map((rol) => rol._id);
+
+  const savedUser = await newUser.save();
+  res
+    .status(200)
+    .json({ message: `se creo el usuario para ${savedUser.email}` });
 }
 
 module.exports = createUser;
