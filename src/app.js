f63bdd0850d9521b2ea5b99ff612783555a5e34f
cristianaquino <ycristian10@gmail.com>
2022-09-29 01:31:01 -0500

    app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // imports
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const fileUpload = require("express-fileupload");
 
@@ -9,6 +10,8 @@ const infoRouter = require("./routes/info.routes");
 const productsRouter = require("./routes/products.routes");
 const authRouter = require("./routes/auth.routes");
 const userRouter = require("./routes/user.routes");
+const folderRouter = require("./routes/folder.routes");
+const chapterRouter = require("./routes/chapter.routes");
 
 const handleErrors = require("./middlewares/handleErrors");
 
@@ -16,6 +19,7 @@ const app = express();
 createRoles();
 
 // middleware
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(
@@ -27,6 +31,8 @@ app.use("/api", infoRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
+app.use("/api/folders", folderRouter);
+app.use("/api/chapters", chapterRouter);
 app.use(handleErrors);
 
 // exports
