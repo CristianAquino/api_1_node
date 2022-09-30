da4a6e6aed972cc07efe64e1a32cee7853649c8c
cristianaquino <ycristian10@gmail.com>
2022-09-29 22:44:35 -0500

    user

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -22,4 +22,25 @@ async function createUser(req, res) {
     .json({ message: `se creo el usuario para ${savedUser.email}` });
 }
 
-module.exports = createUser;
+function getDataUser(req, res, next) {
+  User.findById(req.id)
+    .populate("folders", {
+      name: 1,
+      description: 1,
+      image: 1,
+      chapters: 1,
+      _id: 0,
+    })
+    .populate("roles", { _id: 0 })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: `no se encontro el usuario con id: ${id}` });
+      }
+      return res.status(200).json({ user });
+    })
+    .catch((error) => next(error));
+}
+
+module.exports = { createUser, getDataUser };
