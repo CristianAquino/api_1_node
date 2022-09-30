41996a1bc3207a34c0027672abdb95d30a0a4c68
cristianaquino <ycristian10@gmail.com>
2022-09-30 16:27:28 -0500

    user

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -13,8 +13,10 @@ async function createUser(req, res) {
     password: await User.encryptPassword(password),
   });
 
-  const foundRoles = await Role.find({ name: { $in: roles } });
-  newUser.roles = foundRoles.map((rol) => rol._id);
+  const foundRoles = await Role.findOne({ name: roles[0] });
+  newUser.roles = newUser.roles.concat(foundRoles._id);
+  // const foundRoles = await Role.find({ name: { $in: roles } });
+  // newUser.roles = foundRoles.map((rol) => rol._id);
 
   const savedUser = await newUser.save();
   res
@@ -43,4 +45,53 @@ function getDataUser(req, res, next) {
     .catch((error) => next(error));
 }
 
-module.exports = { createUser, getDataUser };
+async function updateDataUser(req, res, next) {
+  const { body } = req;
+
+  if (body.password) {
+    body.password = await User.encryptPassword(body.password);
+  }
+
+  User.findByIdAndUpdate(req.id, body, { new: true })
+    .then((update) => {
+      if (!update) {
+        return res
+          .status(404)
+          .json({ message: "no se encontro el usuario a actualizar" });
+      }
+      return res.status(202).json(update);
+    })
+    .catch((error) => next(error));
+}
+
+function deleteUserById(req, res) {
+  const { id } = req.params;
+
+  User.findByIdAndDelete(id).then((del) => {
+    if (!del) {
+      return res.status(404).json({ message: "no existe el usuario" });
+    }
+    return res.status(200).json({ message: `user with id: ${id} deleted` });
+  });
+}
+
+async function updateRol(req, res) {
+  const { id } = req.params;
+  const { roles } = req.body;
+  const user = await User.findById(id);
+
+  const foundRoles = await Role.findOne({ name: roles[0] });
+  user.roles = user.roles.concat(foundRoles._id);
+
+  await user.save();
+
+  res.status(201).json({ message: "se agrego un nuevo rol" });
+}
+
+module.exports = {
+  createUser,
+  getDataUser,
+  updateDataUser,
+  deleteUserById,
+  updateRol,
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
 const { verifyToken, isAdmin } = require("../middlewares/authJwt");
-const { checkRolesExisted } = require("../middlewares/verifySignup");
-const { createUser, getDataUser } = require("../controllers/users.controller");
+const {
+  checkRolesExisted,
+  checkDuplicateUsernameOrEmail,
+} = require("../middlewares/verifySignup");
+const {
+  createUser,
+  getDataUser,
+  updateDataUser,
+  deleteUserById,
+  updateRol,
+} = require("../controllers/users.controller");
 
-router.post("/", [verifyToken, isAdmin, checkRolesExisted], createUser);
+router.post(
+  "/",
+  [verifyToken, isAdmin, checkRolesExisted, checkDuplicateUsernameOrEmail],
+  createUser
+);
 router.get("/", verifyToken, getDataUser);
+// usuario comun actualiza sus datos
+router.put("/", [verifyToken], updateDataUser);
+// admin asigna roles
+router.put("/:id", [verifyToken, isAdmin, checkRolesExisted], updateRol);
+router.delete("/:id", [verifyToken, isAdmin], deleteUserById);
 
 module.exports = router;
